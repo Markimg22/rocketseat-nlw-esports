eab85ae554814db862fe99b4f4fc7c43a1ebecea
Markimg22 <marquim.net.221200@hotmail.com>
2022-09-14 17:43:43 -0400

    add eslint and prettier

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,7 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.listen(3000, () => {
-  console.log('Server started!')
-})
+  console.log('Server started!');
+});
